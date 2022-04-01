2f5fc8087e891429fa4af7a355d828acabd567cc
Nigel Mulholland <nigeldave19@gmail.com>
2022-04-01 11:34:23 +0530

    Create oculysTest.groovy

---
+++ src/date_time/oculysTest.groovy
@@ -0,0 +1,11 @@
+package date_time
+
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmm")
+ZoneId zoneId = ZoneId.of("Canada/Eastern")
+LocalDateTime now = LocalDateTime.now(zoneId)
+println (dtf.format(now))
+
