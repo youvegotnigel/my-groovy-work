ace5e601d5dabb85529732afa9ffbe7defdbd0a5
Nigel Mulholland <nigeldave19@gmail.com>
2022-05-16 17:01:14 +0530

    Update CurrentDate.groovy

--- src/date_time/CurrentDate.groovy
+++ src/date_time/CurrentDate.groovy
@@ -1,9 +1,23 @@
 package date_time
 
-import java.text.DateFormat
-import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
-DateFormat format = new SimpleDateFormat("dd-MMM-yyyy")
-Date date = new Date()
-println format.format(date)
 
+for(int i=0; i<=6; i++){
+
+    println getFutureDate(i,0, "MMM yyyy")
+    println getPastDate(i,0, "MMM yyyy")
+}
+
+def getFutureDate(int months, int days, String format) {
+    def date = LocalDate.now(ZoneId.systemDefault()).plusMonths(months).plusDays(days).format(DateTimeFormatter.ofPattern(format))
+    return date
+}
+
+def getPastDate(int months, int days, String format) {
+
+    def date = LocalDate.now(ZoneId.systemDefault()).minusMonths(Math.abs(months)).minusDays(Math.abs(days)).format(DateTimeFormatter.ofPattern(format))
+    return date
+}
