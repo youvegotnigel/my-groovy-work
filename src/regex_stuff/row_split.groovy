97f3a6d237577626e3a2f19116239f4915ff28d9
Nigel Mulholland <nigeldave19@gmail.com>
2022-06-28 19:41:31 +0530

    Update row_split.groovy

--- src/regex_stuff/row_split.groovy
+++ src/regex_stuff/row_split.groovy
@@ -4,15 +4,11 @@ package regex_stuff
 for(int i=1; i<10; i++){
 
     def row_name = "row$i"
-
-    def digit = row_name.findAll("\\d{1,2}")
-
-    int value = (digit[0] as int)*2 -1
+    int val = ((row_name.findAll("\\d{1,2}"))[0] as int)*2 - 1
 
     println("Input  = $row_name")
-    println("Output = row$value")
+    println("Output = row$val")
     println("----------------------")
-
 }
 
 
