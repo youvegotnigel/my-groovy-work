743f8ce9d8fcf90917baac8973130b0f3a3bb2e1
Nigel Mulholland <nigeldave19@gmail.com>
2022-12-25 10:20:50 +0530

    Create Flatern.groovy

---
+++ src/programming/Flatern.groovy
@@ -0,0 +1,11 @@
+package programming
+
+
+
+
+def nestedList = [["SMHC #", "Patient Name", "Birthdate"]]
+
+def flatList = nestedList.flatten()
+
+println(flatList)  // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
